3a1bd3b7dacbfc391a88f060b8dcfb112ee8dfb7
tahdi-islam <tshdi60@gmail.com>
2022-10-19 06:56:58 +0600

    complete wallet style

--- src/Component/Wallet.jsx
+++ src/Component/Wallet.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import image from "../assets/eth.jpg"
 
 const Wallet = () => {
@@ -10,15 +11,15 @@ const Wallet = () => {
                         <img src={image} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
                     </div>
                     <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                        <h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis
-                            <span className="text-purple-600">senectus</span>erat pharetra
+                        <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+                            Balance: 
+                            <span className="text-purple-600"> 3,2244</span>
                         </h1>
-                        <p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-                            <br className="hidden md:inline lg:hidden"/>turpis pulvinar, est scelerisque ligula sem
+                        <p className="mt-6 mb-8 text-lg sm:mb-12">Originally referred to as Ethereum 2.0, the merge is an upgraded version of the Ethereum blockchain that uses a proof-of-stake consensus mechanism to verify transactions via staking.
                         </p>
                         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                            <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-purple-600 text-gray-50">Suspendisse</a>
-                            <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded border-gray-800">Malesuada</a>
+                            <Link rel="noopener noreferrer" to='/spend' className="px-8 py-3 text-lg font-semibold rounded bg-purple-600 text-gray-50">Spend</Link>
+                            <Link rel="noopener noreferrer" to='/earn' className="px-8 py-3 text-lg font-semibold border rounded border-gray-800">Earn</Link>
                         </div>
                     </div>
                 </div>
