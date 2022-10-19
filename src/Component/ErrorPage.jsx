321dd150a3563bcad6b2ddfd7fea8575f0f9f24d
tahdi-islam <tshdi60@gmail.com>
2022-10-19 06:10:10 +0600

    complete error page

--- src/Component/ErrorPage.jsx
+++ src/Component/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
@@ -11,8 +12,8 @@ const ErrorPage = () => {
                         <polygon fill="currentColor" points="210.63 228.042 186.588 206.671 207.958 182.63 184.042 161.37 162.671 185.412 138.63 164.042 117.37 187.958 141.412 209.329 120.042 233.37 143.958 254.63 165.329 230.588 189.37 251.958 210.63 228.042"></polygon>
                         <polygon fill="currentColor" points="383.958 182.63 360.042 161.37 338.671 185.412 314.63 164.042 293.37 187.958 317.412 209.329 296.042 233.37 319.958 254.63 341.329 230.588 365.37 251.958 386.63 228.042 362.588 206.671 383.958 182.63"></polygon>
                     </svg>
-                    <p className="text-3xl">Looks like our services are currently offline</p>
-                    <a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded bg-purple-600 text-gray-50">Back to homepage</a>
+                    <p className="text-3xl">Sorry page not found !!</p>
+                    <Link to='/home' rel="noopener noreferrer" className="px-8 py-3 font-semibold rounded bg-purple-600 text-gray-50">Back to homepage</Link>
                 </div>
             </section>
         </div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../Component/ErrorPage";
 import Home from "../Component/Home";
 import Login from "../Component/Login";
 import Profile from "../Component/Profile";
@@ -9,6 +10,7 @@ import Main from "../Layout/Main";
 
 export const router = createBrowserRouter([
   {path: '/', element: <Main/>, children: [
+    { path: '*', element: <ErrorPage />, },
     {path: '/', element: <Home/>},
     {path: '/home', element: <Home/>},
     {path: '/profile', element: <Profile/>},
